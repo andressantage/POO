2e5014303a31a499a6ec3fc024548ae8e9c1dc02
andressantage <ingeniero.andres.santana@gmail.com>
2023-05-24 15:15:26 -0500

    feat: :art: cambio

---
+++ main.js
@@ -0,0 +1,52 @@
+/**
+ * *La palabra lapiz hace referencia a una clase
+ */
+class lapiz{
+    color
+    tamano
+    #material
+    #mina
+    /**
+     * 
+     * @param {*color} color
+     * @param {*color} color
+     * 
+     */
+    constructor({color="amarillo", tamano="9cm", material="carbon", mina="carbon"}){
+        this.color=color;
+        this.tamano=tamano;
+        this.#material=material;
+        this.#mina=mina;
+    }
+    /**
+     * get llama al resultado
+     */
+    get getMina(){
+        return this.#mina
+    }
+    /**
+     * set permite agregar, eliminar y verificar existencia de un elemento dentro de algo
+     */
+    set setMina(p1){
+        this.#mina=p1;
+    }
+    /**
+     * static es para decir que afilar es un metodo estatico
+     */
+    static afilar(p1){
+        return `la mina de ${p1} ha sido afilada`;
+    }
+}
+let lapiz1=new lapiz({material: "Diamante"});
+let lapiz2=new lapiz({material: "Acero", mina: "grafito"});
+console.log(lapiz1, lapiz2)
+lapiz.setMina="Tinta"
+console.log(lapiz.afilar(lapiz1.getMina));
+console.log(lapiz.afilar(lapiz2.getMina));
+
+
+
+/** Metodos estaticos: es un metodo que pertenece a la clase en si y no a una instancia de la clase
+ * Estos se pueden llamar directamente en la clase sin necesidad de crear un objeto o instancia de la misma
+ *  */ 
+
