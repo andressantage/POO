0fb328b393776a02bab145804f1459ba3a788947
andressantage <ingeniero.andres.santana@gmail.com>
2023-05-24 16:23:01 -0500

    feat: :art: Cambios
    Cambios de la clase de skills may 24/2023

--- main.js
+++ main.js
@@ -33,6 +33,9 @@ class lapiz{
     /**
      * static es para decir que afilar es un metodo estatico
      */
+    /** Metodos estaticos: es un metodo que pertenece a la clase en si y no a una instancia de la clase
+     * Estos se pueden llamar directamente en la clase sin necesidad de crear un objeto o instancia de la misma
+     **/ 
     static afilar(p1){
         return `la mina de ${p1} ha sido afilada`;
     }
@@ -40,13 +43,27 @@ class lapiz{
 let lapiz1=new lapiz({material: "Diamante"});
 let lapiz2=new lapiz({material: "Acero", mina: "grafito"});
 console.log(lapiz1, lapiz2)
-lapiz.setMina="Tinta"
+/**
+ * se agrega a lapiz
+ */
+lapiz1.setMina="Tinta"
+/**
+ * se usa 
+ */
 console.log(lapiz.afilar(lapiz1.getMina));
 console.log(lapiz.afilar(lapiz2.getMina));
 
+/**
+ * el extends se refiere a que extiende las propiedades de lapiz al boligrafo
+ * esto es principalmente para HERENCIA
+ */
+class boligrafo extends lapiz{
+    constructor({mina="Metalica"}){
+        super({mina});
+    }
+}
 
+let obj=new boligrafo({});
+obj.setMina="Aluminio"
 
-/** Metodos estaticos: es un metodo que pertenece a la clase en si y no a una instancia de la clase
- * Estos se pueden llamar directamente en la clase sin necesidad de crear un objeto o instancia de la misma
- *  */ 
-
+console.log(obj.getMina);
